f30cc91e43005b868b5d590e248f2523a0bd0f71
Jegatheeswari.K <jegatheeswarimkovil@gmail.com>
2024-07-09 18:00:12 +0530

    AdditionalInfo.java file added

---
+++ AdditionalInfo.java
@@ -0,0 +1,5 @@
+class AdditionalInfo{
+    public static void main(String[] args) {
+        System.out.println("Understanding the precedence rules helps in writing clear and bug-free Java code, especially when dealing with complex class hierarchies and inheritance.");
+    }
+}
